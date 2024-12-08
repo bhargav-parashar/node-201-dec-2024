8970c26696272321f2cbfd02cb342067fda5284a
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2024-12-08 15:32:25 +0530

    session3
    validations for gender and age

--- session3/controllers/users.controllers.js
+++ session3/controllers/users.controllers.js
@@ -16,6 +16,22 @@ const getUsersById = (req, res) => {
 
 const searchUsers = (req, res) => {
   const { gender, age } = req.query;
+  const validGenders = ["male", "female"];
+
+  if (gender && !validGenders.includes(gender))
+    return res
+      .status(400)
+      .send({ message: `Gender must be one of: ${validGenders.join(", ")}` }); // "male, female"
+  if (
+    (age && isNaN(age)) ||
+    !Number.isInteger(Number(age)) ||
+    parseInt(age) <= 0 ||
+    parseInt(age) > 100
+  )
+    return res
+      .status(400)
+      .send({ message: `Age must be an integer between 1 and 100` });
+
   if (gender && age)
     return res.send(
       data.filter(
