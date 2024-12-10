d7cb3953c1f3a28c589812c0b435fa74313b144d
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2024-12-10 22:44:36 +0530

    session4
    middlewares

---
+++ session4/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require("dotenv").config();
+const currencyRouter = require("./routes/currencies.routes");
+const userRouter = require("./routes/users.routes");
+const authorize = require("./middlewares/authorize");
+
+const server = express();
+const PORT = 8082;
+
+server.get("/", (req, res) => {
+  res.send("<h1>Currency Database</h1>");
+});
+
+server.use("/currencies", currencyRouter);
+
+// server.use(authorize);
+
+server.use("/users", userRouter);
+
+// server.get("/users/:userId/posts/:postId", (req, res) => {
+//   console.log(req.params);
+// });
+
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
